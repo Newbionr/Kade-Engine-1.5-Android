cfebc6554692215ae1cf383c2d4a7643a01e422f
Newbionr <101176446+Newbionr@users.noreply.github.com>
2023-06-10 14:39:20 +0900

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -682,9 +682,9 @@ class PlayState extends MusicBeatState
 						bg.scrollFactor.set(0.9, 0.9);
 						bg.active = false;
 						add(bg);
-					}
-				default:
-					{
+			}
+			case 'stage':
+				{
 						defaultCamZoom = 0.9;
 						curStage = 'stage';
 						var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('stageback'));
